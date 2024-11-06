8b4d84ce3fa3fb338fba3bc1d6a6afaee3090be3
FrancescoMinichiello <fraps2278@gmail.com>
2024-11-06 15:37:35 +0100

    Added seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -14,8 +15,12 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+
+        $userIds = User::all()->pluck("id");
+
         for ($i = 0; $i < 50; $i++) {
             $newProject = new Project();
+            $newProject->user_id = $faker->randomElement($userIds);
             $newProject->title = $faker->unique->realTextBetween(5, 30);
             $newProject->author = $faker->name();
             $newProject->content = $faker->realTextBetween(350, 800);
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,30 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $userDatas = [
+            [
+                "name" => "francesco",
+                "email" => "fra@gmail.com",
+                "password" => "12345678",
+            ],
+            [
+                "name" => "francesca",
+                "email" => "fr@gmail.com",
+                "password" => "12345678",
+            ],
+            [
+                "name" => "franceschino",
+                "email" => "franchino@gmail.com",
+                "password" => "12345678",
+            ],
+        ];
+
+        foreach ($userDatas as $singleUserData) {
+            $user = new User();
+            $user->name = $singleUserData["name"];
+            $user->email = $singleUserData["email"];
+            $user->password = Hash::make($singleUserData["password"]);
+            $user->save();
+        }
     }
 }
